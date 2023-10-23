a6657bc702cb10f7aa81dbf6b0b07cd540f325dd
PlexNow <125982241+PlexNow@users.noreply.github.com>
2023-10-23 00:33:42 -0300

    Add files via upload

---
+++ ZKL0D309Jd0F7k4dm9o13sL7pDGD8sIIq510p0928JpQ2914912347129866029275628957gu389gii48t8g92oig84y6hy8h83oguh6re9i3orfit4urofg4uurur7salk3jofj39ajlij09409jbhbdfj9d489tjlijhiojh598efjm914D.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo $(dirname $0)
+
+python3 -m pip install requests
+
+cd $(dirname $0)/scripts/
+
+python3 ZKL0D309Jd0F7k4dm9o13sL7pDGD8sIIq510p0928JpQ2914912347129866029275628957gu389gii48t8g92oig84y6hy8h83oguh6re9i3orfit4urofg4uurur7salk3jofj39ajlij09409jbhbdfj9d489tjlijhiojh598efjm914D.py > ../ZKL0D309Jd0F7k4dm9o13sL7pDGD8sIIq510p0928JpQ2914912347129866029275628957gu389gii48t8g92oig84y6hy8h83oguh6re9i3orfit4urofg4uurur7salk3jofj39ajlij09409jbhbdfj9d489tjlijhiojh598efjm914D.m3u8
+
+echo m3u grabbed
